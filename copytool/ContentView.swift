c4ec22f68039260be08349ba155c838c8165d8a9
yexiangbin <xiangbin.ye@biz-united.com.cn>
2026-03-12 17:57:53 +0800

    Initial Commit

---
+++ copytool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  copytool
+//
+//  Created by yuhoo on 2026/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ copytool/copytoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  copytoolApp.swift
+//  copytool
+//
+//  Created by yuhoo on 2026/3/12.
+//
+
+import SwiftUI
+
+@main
+struct copytoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
